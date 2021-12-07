7ebe47a9ac23fcee570510aec82c0271cc9a65a4
CapralDavid <ssdrevcukss@gmail.com>
2021-12-07 03:29:23 +0100

    Some changes

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,25 +3,83 @@ package main
 import (
 	"flag"
 	"fmt"
-	lab2 "github.com/roman-mazur/architecture-lab-2"
+	"strings"
+	lab2 "github.com/CapralDavid/Architecture2"
 )
-
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
 	// TODO: Add other flags support for input and output configuration.
 )
 
-func main() {
+func isOperand(x string) (bool){
+	switch x{
+	case "+", "-", "*", "/", "^":
+		return true
+
+	default:
+		return false
+
+	}
+
+}
+
+//вынимаем из стака два последних элемента
+func popTwoFromArray(stack []string) ([]string, string, string) {
+	var ab []string
+	stack, ab = stack[:len(stack)-2], stack[len(stack)-2:]
+	return stack, ab[0], ab[1]
+}
+
+
+
+func main(){
+	fmt.Println("hello there")
+
+	var stack []string
+	//var value float64
+
+    st:="* + 5 3 - 81 15"
+	var symbols = strings.Fields(st)
+
+	for i:=len(symbols)-1; i>=0; i--{
+		fmt.Println(symbols[i])
+
+		if isOperand(symbols[i]){
+			var one, two string
+			fmt.Println("OPERAND")
+			//НЕТ ПРОВЕРКИ НА ОШИБКУ
+			/*
+			нужно что бы когда попадается операнда в стаке уже должно быть 
+			2 числа.а то если будет - и только одно число то тогда как
+			считать?
+			что то в этом роде 				
+			if len(stack)< 2{
+			print
+			}
+
+			*/
+			stack, two, one = popTwoFromArray(stack)
+			fmt.Println("(" + one + symbols[i] + two + ")")
+			otvet:="(" + one + symbols[i] + two + ")"
+			stack = append(stack, otvet)
+
+
+
+		
+		}else{
+			stack = append(stack, symbols[i])
+		}
+
+		fmt.Println("________THIS IS STACK NOW_______")
+		fmt.Println(stack)
+		fmt.Println("____THIS IS STACK NOW_____end_____")
+
+	}
+	func main() {
+		flag.Parse()
+	fmt.Println(len(symbols))
+	fmt.Println(stack)
 	flag.Parse()
-
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
-
 	res, _ := lab2.PrefixToPostfix("+ 2 2")
 	fmt.Println(res)
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
 // TODO: Rename to your own module.
-module github.com/roman-mazur/architecture-lab-2
+module github.com/CapralDavid/Architecture2
 
 go 1.17
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/roman-mazur/architecture-lab-2 v0.0.0-20211005190120-48dabf144c73 // indirect
 	// TODO: Change if need to use a different assertions library.
 	github.com/stretchr/testify v1.7.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
