db3a5101ef7241d90ea8b16d3179934abaa7bb7b
Bessonica <studies.vuz@gmail.com>
2021-12-21 02:17:24 +0200

    command line argument added

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,17 +3,36 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"os"
+	"strings"
 	lab2 "github.com/CapralDavid/Architecture2"
 )
 
 
 var (
-	inputExpression = flag.String("e", "", "Expression to compute")
+	inputExpression = flag.String("e", "", "Expression to evaluate")
 	// TODO: Add other flags support for input and output configuration.
 )
 
 func  main() {
 	flag.Parse()
+
+	var input io.Reader = nil
+	var output = os.Stdout
+
+	if *inputExpression !="" {
+		input= strings.NewReader(*inputExpression)
+	}
+
+	handler := &lab2.ComputeHandler{
+		Input:  input,
+		Output: output,
+	}
+	err := handler.Compute()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
 	// TODO: Change this to accept input from the command line arguments as described in the task and
 	//       output the results using the ComputeHandler instance.
@@ -23,8 +42,8 @@ func  main() {
 	//       }
 	//       err := handler.Compute()
 
-	res, _ := lab2.TurnPrefixIntoInfix("* + 5 3 - 81 15")
-	fmt.Println(res)
+	//res, _ := lab2.TurnPrefixIntoInfix("* + 5 3 - 81 15")
+	//fmt.Println(res)
 }
 
 
